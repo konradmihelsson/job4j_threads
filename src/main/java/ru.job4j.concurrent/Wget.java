002230460722b717814132ada439596844d5948e
konradmihelsson <konrad.mihelsson@gmail.com>
2021-07-09 13:09:56 +0300

    3. Уровень - Джуниор. Блок 3.1.1. Threads. 3.1. Прерывание блокированной нити. [#267413]

--- src/main/java/ru.job4j.concurrent/Wget.java
+++ src/main/java/ru.job4j.concurrent/Wget.java
@@ -1,22 +1,23 @@
 package ru.job4j.concurrent;
 
 public class Wget {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(
                 () -> {
                     try {
                         System.out.println("Start loading ... ");
                         for(int i = 0; i <= 100; i++) {
                             System.out.print("\rLoading : " + i + "%");
-                            Thread.sleep(1000);
+                            Thread.sleep(10);
                         }
                         System.out.println(" Loaded.");
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
                     }
                 }
         );
         thread.start();
-        System.out.println("Main");
+        thread.join();
+        System.out.println("Main complete");
     }
 }
