4c932d7ec73720403b6b2c6e70f5a273d5231150
konradmihelsson <konrad.mihelsson@gmail.com>
2021-07-09 19:06:50 +0300

    3. Уровень - Джуниор. Блок 3.1.1. Threads. 4. Скачивание файла с ограничением. [#144271 #175808] (add args validate; saved file get filename from URL string)

--- src/main/java/ru.job4j.concurrent/WgetLimited.java
+++ src/main/java/ru.job4j.concurrent/WgetLimited.java
@@ -3,11 +3,13 @@ package ru.job4j.concurrent;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Paths;
 
 public class WgetLimited implements Runnable {
 
-    private final long SEC_IN_MILLISEC = 1000;
     private final String url;
     private final int speed;
 
@@ -19,7 +21,8 @@ public class WgetLimited implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(this.url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream =
+                     new FileOutputStream(Paths.get(new URI(url).getPath()).getFileName().toString())) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             long time = System.currentTimeMillis();
@@ -28,13 +31,14 @@ public class WgetLimited implements Runnable {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 long diffTime = System.currentTimeMillis() - time;
                 bytesPerSecWrited = bytesPerSecWrited + 1024;
+                long SEC_IN_MILLISEC = 1000;
                 if (bytesPerSecWrited > this.speed && diffTime < SEC_IN_MILLISEC) {
                     Thread.sleep(SEC_IN_MILLISEC - diffTime);
                     bytesPerSecWrited = 0;
                     time = System.currentTimeMillis();
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -42,10 +46,21 @@ public class WgetLimited implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        argsValidator(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new WgetLimited(url, speed));
         wget.start();
         wget.join();
+    }
+
+    private static void argsValidator(String[] args) {
+        if (args.length != 2) {
+            String ls = System.lineSeparator();
+            String sb = "Wrong arguments. Usage: WgetLimited url rate" + ls +
+                    "when url - file http(s) web address, rate - download speed limin in byte per second" + ls +
+                    "For example: WgetLimited https://proof.ovh.net/files/1Mb.dat 262144" + ls;
+            throw new IllegalArgumentException(sb);
+        }
     }
 }
