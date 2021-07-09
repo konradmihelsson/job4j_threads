9a3e5c132099404c63ea0d26208384a1a398dea6
konradmihelsson <konrad.mihelsson@gmail.com>
2021-07-09 20:56:34 +0300

    3. Уровень - Джуниор. 3.1.2. Общие ресурсы. 3. Immutable объекты [#267918]

---
+++ src/main/java/ru/job4j/immutable/Node.java
@@ -0,0 +1,19 @@
+package ru.job4j.immutable;
+
+public class Node<T> {
+    private final T value;
+    private final Node<T> next;
+
+    public Node(T value, Node<T> next) {
+        this.value = value;
+        this.next = new Node<>(next.getValue(), next.getNext());
+    }
+
+    public Node<T> getNext() {
+        return new Node<>(this.next.getValue(), this.next.getNext());
+    }
+
+    public T getValue() {
+        return this.value;
+    }
+}
