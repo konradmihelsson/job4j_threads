8a63b7060362b1645da901cb68ff64117d29b16d
konradmihelsson <konrad.mihelsson@gmail.com>
2021-07-12 21:23:09 +0300

    Уровень : 3. Мидл. Категория : 3.1. Multithreading. Топик : 3.1.4. Wait, Notify, NotifyAll. 0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/interthread/CountBarrier.java
@@ -0,0 +1,35 @@
+package ru.job4j.interthread;
+
+public class CountBarrier {
+
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            this.count++;
+            if (this.count >= this.total) {
+                monitor.notifyAll();
+            }
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (this.count < this.total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
