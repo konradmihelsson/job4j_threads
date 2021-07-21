89d21efdd84db257145714ba73ed3fa08ac009f6
konradmihelsson <konrad.mihelsson@gmail.com>
2021-07-21 11:07:49 +0300

    Уровень : 3. Мидл. Категория : 3.1. Multithreading. Топик : 3.1.5. Non Blocking Algorithm. 1. Неблокирующий кеш [#4741 #179824]

--- src/main/java/ru/job4j/cache/Cache.java
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -11,18 +11,16 @@ public class Cache {
     }
 
     public boolean update(Base model) {
-        return !(memory.computeIfPresent(model.getId(), (key, value) -> compute(model)) == null);
+        return memory.computeIfPresent(model.getId(), (key, value) -> {
+                    if (model.getVersion() != value.getVersion()) {
+                        throw new OptimisticException("Versions are not equal");
+                    }
+                    return model;
+        }
+        ) != null;
     }
 
     public void delete(Base model) {
         memory.remove(model.getId(), model);
-    }
-
-    private Base compute(Base model) {
-        Base stored = memory.get(model.getId());
-        if (stored.getVersion() != model.getVersion()) {
-            throw new OptimisticException("Versions are not equal");
-        }
-        return model;
     }
 }
